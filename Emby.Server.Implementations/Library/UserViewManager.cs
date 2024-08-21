a58012063208a3ff74e0a980e24b29fab9aedb7c
LJQ <leejunquan7@gmail.com>
2024-08-21 19:32:33 +0800

    Try fix latest endpoint
    (cherry picked from commit da313e5e878c7eba35fffd7dfe053688b66cad0a)

--- Emby.Server.Implementations/Library/UserViewManager.cs
+++ Emby.Server.Implementations/Library/UserViewManager.cs
@@ -383,6 +383,18 @@ namespace Emby.Server.Implementations.Library
                 return _libraryManager.GetItemList(query, false);
             }
 
+            if (parents.OfType<ICollectionFolder>().All(i => i.CollectionType == CollectionType.tvshows))
+            {
+                query.Limit = limit;
+                return _libraryManager.GetItemList(query, parents, CollectionType.tvshows);
+            }
+
+            if (parents.OfType<ICollectionFolder>().All(i => i.CollectionType == CollectionType.music))
+            {
+                query.Limit = limit;
+                return _libraryManager.GetItemList(query, parents, CollectionType.music);
+            }
+
             return _libraryManager.GetItemList(query, parents);
         }
     }
--- MediaBrowser.Controller/Persistence/IItemRepository.cs
+++ MediaBrowser.Controller/Persistence/IItemRepository.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using Jellyfin.Data.Enums;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Model.Dto;
 using MediaBrowser.Model.Entities;
@@ -132,6 +133,14 @@ namespace MediaBrowser.Controller.Persistence
         /// <param name="query">The query.</param>
         /// <returns>List&lt;BaseItem&gt;.</returns>
         List<BaseItem> GetItemList(InternalItemsQuery query);
+
+        /// <summary>
+        /// Gets the item list.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        /// <param name="collectionType">Type of the collection.</param>
+        /// <returns>List&lt;BaseItem&gt;.</returns>
+        List<BaseItem> GetLatestItemList(InternalItemsQuery query, CollectionType collectionType);
 
         /// <summary>
         /// Updates the inherited values.
